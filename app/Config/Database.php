7f57af7f16c970b2c1d342c13a627cd07970f209
Dámilólá Ọwádayọ̀ 💻 <77008627+ItzSamdam@users.noreply.github.com>
2025-09-26 13:16:31 +0100

    added config folder - databse file

---
+++ app/Config/Database.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Config;
+
+class Database {
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    public $conn;
+
+    public function __construct() {
+        $this->host = $_ENV['DB_HOST'] ?? 'localhost';
+        $this->db_name = $_ENV['DB_NAME'] ?? 'finance_api';
+        $this->username = $_ENV['DB_USER'] ?? 'root';
+        $this->password = $_ENV['DB_PASS'] ?? '';
+    }
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new \PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8",
+                $this->username,
+                $this->password,
+                [
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                    \PDO::ATTR_EMULATE_PREPARES => false
+                ]
+            );
+        } catch(\PDOException $exception) {
+            throw new \Exception("Connection error: " . $exception->getMessage());
+        }
+        return $this->conn;
+    }
+}
+?>
